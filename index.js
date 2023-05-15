6dcc470c38ea9b8d0010918b17df64be45719e57
Umesh Patil <100535094+umyapatil@users.noreply.github.com>
2023-05-16 01:08:35 +0530

    weeding clone website

---
+++ index.js
@@ -0,0 +1,13 @@
+var menuBtn = document.querySelector(".res-icon");
+var options = document.querySelector(".options");
+
+
+menuBtn.addEventListener("click",function(){
+    
+   if(options.style.display ="none"){
+        options.style.display ="inline-block"; 
+    }
+
+})
+
+
